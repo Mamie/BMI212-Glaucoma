edf7d45ff5cd9eed7b318e53217df6eabc693b69
mamiewang <szmamie@live.com>
2017-06-04 17:40:36 -0700

    Update the model formula

--- script/aim1.R
+++ script/aim1.R
@@ -13,12 +13,11 @@ data.all$BMI <- as.numeric(scale(data.all$BMI))
 data.all$NDD <- apply(cbind(data.all$ALZHEIM, data.all$PARKINS, data.all$ALS, data.all$MS), 
                       1, function(x) x[1] | x[2] | x[3] | x[4])
 
-
 # baseline model
-# model.baseline <- glmer(formula = NDD ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
-#                        data=data.all, na.action=na.omit,
-#                        family=binomial(link = "logit"), verbose=1)
-# summary(model.baseline)
+model.baseline <- glmer(formula = NDD ~ AGE + diab + RACE + BMI + diab:AGE + (1 | ID), 
+                        data=data.all, na.action=na.omit,
+                        family=binomial(link = "logit"), verbose=1)
+summary(model.baseline)
 
 # full model
 model.NDD <- glmer(formula = NDD ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
@@ -27,4 +26,4 @@ model.NDD <- glmer(formula = NDD ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE 
 summary(model.NDD)
 
 write.table(data.all, file='NDDPSM.csv', sep=',', row.names=F, col.names=T, quote=F)
-save(model.NDD, 'specificAim1.RData')
+save(model.baseline, file='specificAim1.RData')
--- script/aim2.R
+++ script/aim2.R
@@ -2,57 +2,57 @@ library(lme4)
 library(dplyr)
 
 # specific aim 2
-data.all <- read.csv('NDDPSM.csv', stringsAsFactors=F)
-
-
-# baseline models
-alz.baseline <- glmer(formula = ALZHEIM ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
-                        data=data.all, na.action=na.omit,
-                        family=binomial(link = "logit"), verbose=1)
-summary(alz.baseline)
-
-
-parkins.baseline <- glmer(formula = PARKINS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
-                          data=data.all, na.action=na.omit,
-                          family=binomial(link = "logit"), verbose=1)
-summary(parkins.baseline)
-
-
-als.baseline <- glmer(formula = ALS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
-                      data=data.all, na.action=na.omit,
-                      family=binomial(link = "logit"), verbose=1)
-summary(als.baseline)
-
-
-ms.baseline <- glmer(formula = MS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
-                     data=data.all, na.action=na.omit,
-                     family=binomial(link = "logit"), verbose=1)
-summary(ms.baseline)
-
-save(alz.baseline, parkins.baseline, als.baseline, ms.baseline, 'specificAim1baseline.RData')
+data.all <- read.csv('NDDPSM2.csv', stringsAsFactors=F)
 
 
 # full model
-alz.full <- glmer(formula = ALZHEIM ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
+alz.full <- glmer(formula = ALZHEIM ~ AGE + diab + BMI + RACE + diab:AGE + (1 | ID), 
                   data=data.all, na.action=na.omit, 
                   family=binomial(link = "logit"), verbose=1)
 summary(alz.full)
 
 
-parkins.full <- glmer(formula = PARKINS ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
+parkins.full <- glmer(formula = PARKINS ~ AGE + diab + BMI + RACE + diab:AGE + (1 | ID), 
                       data=data.all, na.action=na.omit, 
                       family=binomial(link = "logit"), verbose=1)
 summary(parkins.full)
 
-als.full <- glmer(formula = ALS ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
+als.full <- glmer(formula = ALS ~ AGE + diab + BMI + RACE + diab:AGE + (1 | ID), 
                   data=data.all, na.action=na.omit, 
                   family=binomial(link = "logit"), verbose=1)
 summary(als.full)
 
-ms.full <- glmer(formula = MS ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
+ms.full <- glmer(formula = MS ~ AGE + diab + BMI + RACE + diab:AGE + (1 | ID), 
                  data=data.all, na.action=na.omit, 
                  family=binomial(link = "logit"), verbose=1)
 summary(ms.full)
 
 
-save(alz.full, parkins.full, als.full, ms.full, 'specificAim1.RData')
+save(alz.full, parkins.full, als.full, ms.full, file='specificAim2.RData')
+
+
+# baseline models
+#alz.baseline <- glmer(formula = ALZHEIM ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
+#                      data=data.all, na.action=na.omit,
+#                      family=binomial(link = "logit"), verbose=1)
+#summary(alz.baseline)
+
+
+#parkins.baseline <- glmer(formula = PARKINS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
+#                          data=data.all, na.action=na.omit,
+#                          family=binomial(link = "logit"), verbose=1)
+#summary(parkins.baseline)
+
+
+#als.baseline <- glmer(formula = ALS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
+#                      data=data.all, na.action=na.omit,
+#                      family=binomial(link = "logit"), verbose=1)
+#summary(als.baseline)
+
+
+#ms.baseline <- glmer(formula = MS ~ AGE + diab + RACE + diab:AGE + (1 | ID), 
+#                     data=data.all, na.action=na.omit,
+#                     family=binomial(link = "logit"), verbose=1)
+#summary(ms.baseline)
+
+#save(alz.baseline, parkins.baseline, als.baseline, ms.baseline, file='specificAim2.RData')
