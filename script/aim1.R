b390b9bfd4aeb03dd38ad26788cbe1f35c0a9f19
Masood Malekghassemi <soltanmm@gmail.com>
2017-06-05 12:54:28 -0700

    Comment out aim 1's full model
    It doesn't converge.

--- script/aim1.R
+++ script/aim1.R
@@ -12,22 +12,23 @@ data.all <- read.csv('WHImerged.csv', stringsAsFactors=F)
 data.all <- data.all %>%
   mutate(AGE=AGE*365+date) %>%
   filter(ID %in% unlist(matched))
-data.all$AGE <-  as.numeric(scale(data.all$AGE))
+data.all$AGE <- as.numeric(scale(data.all$AGE))
 data.all$BMI <- as.numeric(scale(data.all$BMI))
 data.all$NDD <- apply(cbind(data.all$ALZHEIM, data.all$PARKINS, data.all$ALS, data.all$MS), 
                       1, function(x) x[1] | x[2] | x[3] | x[4])
+write.table(data.all, file='NDDPSM.csv', sep=',', row.names=F, col.names=T, quote=F)
 
 # baseline model
 model.baseline <- glmer(formula = NDD ~ AGE + diab + RACE + BMI + diab:AGE + (1 | ID), 
                         data=data.all, na.action=na.omit,
                         family=binomial(link = "logit"), verbose=1)
 summary(model.baseline)
-
-# full model
-model.NDD <- glmer(formula = NDD ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
-                   data=data.all, na.action=na.omit, 
-                   family=binomial(link = "logit"), verbose=1)
-summary(model.NDD)
-
-write.table(data.all, file='NDDPSM.csv', sep=',', row.names=F, col.names=T, quote=F)
 save(model.baseline, file='specificAim1.RData')
+
+# full model (does not converge)
+#model.NDD <- glmer(formula = NDD ~ AGE + diab + BMI + RACE + diab:AGE + BMI:AGE + (1 | ID), 
+#                   data=data.all, na.action=na.omit, 
+#                   family=binomial(link = "logit"), verbose=1)
+#summary(model.NDD)
+#save(model.baseline, model.NDD, file='specificAim1.RData')
+
